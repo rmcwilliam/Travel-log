f090f6e4f996a8f5c513bbca45da81ec628c39db
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-14 17:18:34 -0500

    started logs controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -14,3 +14,12 @@ class LogsController < ApplicationController
   def destroy
   end
 end
+
+
+
+
+GET    /users/:id/logs(.:format)               logs#index
+POST   /logs(.:format)                         logs#create
+GET    /logs/:id(.:format)                     logs#show
+PUT    /logs/:id(.:format)                     logs#update
+DELETE /logs/:id(.:format)                     logs#destroy
