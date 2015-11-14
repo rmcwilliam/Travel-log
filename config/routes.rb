91a085d02e6affc10914673ce84ad47a44f5433c
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-14 17:30:26 -0500

    wtf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-
+Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -64,4 +64,4 @@
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+end
