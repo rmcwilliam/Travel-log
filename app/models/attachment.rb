6ab8e3bb64fb758e26b59c367b0f568ac1607d60
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-14 14:31:51 -0500

    added validations

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,9 @@
 class Attachment < ActiveRecord::Base
 	belongs_to :log
 	belongs_to :user
+
+  has_attached_file :attachment
+  validates :attachment, attachment_presence: true
+
+  validates_presence_of :log_id  
 end
