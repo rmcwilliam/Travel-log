47e6516ecff2b8ba6e3e3346d759b31cee7287a5
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-14 18:24:37 -0500

    commented out

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -18,8 +18,8 @@ end
 
 
 
-GET    /users/:id/logs(.:format)               logs#index
-POST   /logs(.:format)                         logs#create
-GET    /logs/:id(.:format)                     logs#show
-PUT    /logs/:id(.:format)                     logs#update
-DELETE /logs/:id(.:format)                     logs#destroy
+# GET    /users/:id/logs(.:format)               logs#index
+# POST   /logs(.:format)                         logs#create
+# GET    /logs/:id(.:format)                     logs#show
+# PUT    /logs/:id(.:format)                     logs#update
+# DELETE /logs/:id(.:format)                     logs#destroy
