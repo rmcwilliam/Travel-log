8e1d9c96238ef07d7fa1d995ab5cc92589794d4b
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-14 15:08:53 -0500

    fixed spelling error

--- db/migrate/20151114173430_create_logs.rb
+++ db/migrate/20151114173430_create_logs.rb
@@ -1,7 +1,7 @@
 class CreateLogs < ActiveRecord::Migration
   def change
     create_table :logs do |t|
-      t.stsring :title, null: false
+      t.string :title, null: false
       t.string :description
       t.integer :user_id, null: false
       t.integer :attachment_id, null: false
