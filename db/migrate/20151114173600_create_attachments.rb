50b90a59af95fe8a6ef66a3306e2d87b87038883
Ryan McWilliam <ryanmcwilliam@gmail.com>
2015-11-14 15:04:53 -0500

    added id's and t.attachment

--- db/migrate/20151114173600_create_attachments.rb
+++ db/migrate/20151114173600_create_attachments.rb
@@ -1,9 +1,12 @@
 class CreateAttachments < ActiveRecord::Migration
   def change
     create_table :attachments do |t|
-      t.string :location
-      t.integer :timestamp
-      t.string :caption
+      t.string :location, null: false 
+      t.integer :timestamp, null: false
+      t.string :caption, null: false
+      t.integer :log_id, null: false 
+      t.integer :user_id, null: false
+      t.attachment :attachment, null: false 
 
       t.timestamps null: false
     end
